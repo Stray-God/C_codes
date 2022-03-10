bf17ca931f7397f9d30567348ab5d20e4f55382a
Noragami <68288912+Stray-God@users.noreply.github.com>
2022-03-10 18:08:54 +0530

    print A to Z

---
+++ print A to Z.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    char c;
+    c='A';
+    while (c <='Z')
+    {
+        printf("%c  ",c);
+        c++;
+    }
+}
+
+/*
+#include <stdio.h>
+
+int main()
+{
+    int c;
+    c = 65;
+    while (c <= 'Z')
+    {
+        printf("%c  ",c);
+        c++;
+    }
+}
+*/
