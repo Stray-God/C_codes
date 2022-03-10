87765cd34c73c0227cd73ba6d0ae02ec7e1ab29d
Stray-God <kowshikdiamonds@gmail.com>
2022-03-10 17:40:35 +0530

    whether a number is -ve,+ve,zero

---
+++ number_+or-.c
@@ -0,0 +1,19 @@
+#include"stdio.h"
+
+int main()
+{
+ int a;
+ printf("enter a : ");
+ scanf("%d",&a);
+ 
+ if(a>0)
+ 
+ printf("positive",a);
+ 
+ else if(a<0)
+ printf("negative",a);
+
+ else
+ 
+ printf("zero%d",a);
+}
