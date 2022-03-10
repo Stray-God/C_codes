c71154c33d6dccfb0d96866f906bf02874fdc43b
Noragami <68288912+Stray-God@users.noreply.github.com>
2022-03-10 18:16:29 +0530

    ex1

---
+++ ex1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,n;
+    scanf("%d",&n);
+    for(i=0;printf("hi")>i;i++){ //hi =2 , hii=3
+        printf("%d ",i);
+        
+    }
+
+    return 0;
+}
