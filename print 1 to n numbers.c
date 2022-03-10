60402070b9b0131a0bbf1070ee8380bc2328ca19
Noragami <68288912+Stray-God@users.noreply.github.com>
2022-03-10 18:04:03 +0530

    print 1 to n numbers

---
+++ print 1 to n numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,i;
+    i=1;
+    printf ("enter number :");
+    scanf("%d",&a);
+    
+    while (i<=a)
+    {
+        printf("%d\t",i);
+        i++;
+
+    }
+
+    return 0;
+}
